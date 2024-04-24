66d463cd39e7919925c293ddf8cdec9aabdd5a75
thaua-arruda <thauafarruda@gmail.com>
2024-04-24 16:53:03 -0400

    adicionado questao 1

---
+++ questao1.js
@@ -0,0 +1,9 @@
+/*
+Questão 01: Escreva uma arrow function chamada dobro que recebe um número 
+como parâmetro e retorna o dobro desse número.
+*/
+
+const dobro = x => {
+    return x + x;   
+}
+console.log(dobro(3))
