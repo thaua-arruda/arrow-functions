93417f3a1bf6c3108316ff3ae4216e6c4d15b63e
thaua-arruda <thauafarruda@gmail.com>
2024-04-24 16:53:12 -0400

    adicionado questao 2

---
+++ questao2.js
@@ -0,0 +1,9 @@
+/*
+Questão 02: Escreva uma arrow function chamada saudacao que recebe um nome 
+como parâmetro e retorna uma string de saudação. 
+*/
+
+let saudacao = nome => {
+    return "deitei " + nome ;
+}
+console.log(saudacao('capa'))
