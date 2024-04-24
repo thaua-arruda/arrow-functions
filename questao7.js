273f72343bdb1730bcf0cadb6387c11098849334
thaua-arruda <thauafarruda@gmail.com>
2024-04-24 16:53:35 -0400

    adicionado questao 7

---
+++ questao7.js
@@ -0,0 +1,10 @@
+/*
+Questão 07: Escreva uma arrow function chamada contaLetras que recebe uma 
+string como parâmetro e retorna um objeto contendo a contagem de cada letra 
+presente na string.
+*/
+
+const contaLetras = (x) => {
+    return x.length;
+}
+console.log(contaLetras("Parabéns, você não ganhou"))
